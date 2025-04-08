abece06c6a92390e2caeca8a53af5ab1d271fb2c
Hiccup Enthusiast <hicster.enthusiast@gmail.com>
2025-04-07 23:59:47 -0500

    feat: Refine player detection, split collision in Body Hurtbox and Interaction

--- world/transition_tile.gd
+++ world/transition_tile.gd
@@ -6,19 +6,19 @@ class_name TransitionTile extends Area2D
 var is_occupied: bool = false
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("confirm") and is_occupied:
 		SceneSwticher.goto_scene("res://world/%s.tscn" % target_scene_path, target_door)
 
-func _on_body_entered(body: Node2D) -> void:
-	var parent = body.get_parent()
+func _on_area_entered(body: Area2D) -> void:
+	var parent = body.get_parent().get_parent()
 	if parent is Player:
 		is_occupied = true
 
-func _on_body_exited(body: Node2D) -> void:
-	var parent = body.get_parent()
+func _on_area_exited(body: Area2D) -> void:
+	var parent = body.get_parent().get_parent()
 	if parent is Player:
 		is_occupied = false
