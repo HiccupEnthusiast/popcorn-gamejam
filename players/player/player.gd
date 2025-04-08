1b0f3b97cae5dfe62612560cd4ccee0517e42a1b
Hiccup Enthusiast <hicster.enthusiast@gmail.com>
2025-04-08 11:28:22 -0500

    feat: Generalize interactable objects implementation

--- players/player/player.gd
+++ players/player/player.gd
@@ -1,5 +1,19 @@
 class_name Player extends Entity
 
+@onready var interaction_area: Area2D = $Body/InteractionArea
+var interaction_spots: Array[Interactable] = []
+
+func _ready() -> void:
+	interaction_area.area_entered.connect(_on_interactable_entered)
+	interaction_area.area_exited.connect(_on_interactable_exited)
+
+func _on_interactable_entered(interactable: Interactable) -> void:
+	if interactable == null: return
+	interaction_spots.append(interactable)
+func _on_interactable_exited(interactable: Interactable) -> void:
+	if interactable == null: return
+	interaction_spots.erase(interactable)
+
 func _physics_process(_delta: float) -> void:
 	body.move_and_slide()
 
@@ -16,4 +30,7 @@ func _unhandled_input(_event: InputEvent) -> void:
 	if vel.length() > 0:
 		vel = vel.normalized() * 500.0
 	body.velocity = vel
+
+	if Input.is_action_just_pressed("confirm") and interaction_spots.size() > 0:
+		interaction_spots[-1]._interact()
 
--- world/transition_tile.gd
+++ world/transition_tile.gd
@@ -1,24 +1,8 @@
-class_name TransitionTile extends Area2D
+class_name TransitionTile extends Interactable
 
 @export var spawn_point: Node2D
 @export var target_scene_path: String 
 @export var target_door: String
-var is_occupied: bool = false
 
-func _ready() -> void:
-	area_entered.connect(_on_area_entered)
-	area_exited.connect(_on_area_exited)
-
-func _unhandled_input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("confirm") and is_occupied:
-		SceneSwticher.goto_scene("res://world/%s.tscn" % target_scene_path, target_door)
-
-func _on_area_entered(body: Area2D) -> void:
-	var parent = body.get_parent().get_parent()
-	if parent is Player:
-		is_occupied = true
-
-func _on_area_exited(body: Area2D) -> void:
-	var parent = body.get_parent().get_parent()
-	if parent is Player:
-		is_occupied = false
+func _interact() -> void:
+	SceneSwticher.goto_scene("res://world/%s/%s.tscn" % [target_scene_path, target_scene_path], target_door)
